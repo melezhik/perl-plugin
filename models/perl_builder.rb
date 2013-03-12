9d58c910d191721deda9b7854416008b29dcb91b
Cepnoy <g.rusakov@adriver.ru>
2013-03-12 11:20:28 +0400

    fix interpolation

--- models/perl_builder.rb
+++ models/perl_builder.rb
@@ -57,6 +57,7 @@ class PerlBuilder < Jenkins::Tasks::Builder
         else
             string = "#{key} #{value}"
         end
+        string
     end
     def perform(build, launcher, listener)
 
@@ -182,7 +183,7 @@ class PerlBuilder < Jenkins::Tasks::Builder
                 # basename of distributive will be added to artifatcs
                 distro_url = "#{env['JENKINS_URL']}/job/#{job}/#{build_number}/artifact/#{@dist_dir}/#{distroname}"
                 File.open("#{workspace}/#{@dist_dir}/distro.url", 'w') { |f| f.write(distro_url) }
-                listener.info formatted_text("distro.url:","#{distro_url}")
+                listener.info formatted_text('distro.url:',distro_url)
             end
 
         end # if @enabled == true
